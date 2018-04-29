339d8d0aae8e03c679187d7202070425a578eedc
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2018-04-29 11:52:25 +0700

    Add maps Activity

--- app/src/main/java/com/arifinfrds/papblprojectakhir/ui/MainActivity.kt
+++ app/src/main/java/com/arifinfrds/papblprojectakhir/ui/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.arifinfrds.papblprojectakhir
+package com.arifinfrds.papblprojectakhir.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.arifinfrds.papblprojectakhir.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,6 +21,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
